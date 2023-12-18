88a596b4309eaad43bca0d89e2dbbf94989782bf
Sara Vieira <hey@iamsaravieira.com>
2023-12-18 21:06:32 +0000

    finish

--- workers/src/index.js
+++ workers/src/index.js
@@ -4,14 +4,14 @@ import { makePost, makeProduct } from './lib/make-product';
 import { screenshotPage } from './routes/screenshot';
 
 export default {
-	async fetch(request, env, ctx) {
+	async fetch(request, env) {
 		const router = Router()
 			.get('/html', async () => {
 				const post = await makeProduct().then(makePost);
 				return HTMLRoute(post);
 			})
 			.get('/', async (_, env) => {
-				const page = await screenshotPage(env);
+				const page = await screenshotPage(env, request.url);
 				return page;
 			})
 			.all('*', () => new Response('Not Found.', { status: 404 }));
--- workers/src/routes/screenshot.ts
+++ workers/src/routes/screenshot.ts
@@ -1,12 +1,13 @@
 import puppeteer from '@cloudflare/puppeteer';
 
-export const screenshotPage = async (env) => {
+export const screenshotPage = async (env, url) => {
 	let img;
 
 	const browser = await puppeteer.launch(env.MYBROWSER);
 	const page = await browser.newPage();
 	await page.setViewport({ width: 4400, height: 2500 });
-	await page.goto('/html');
+	await page.goto(url + 'html');
+	await page.waitForSelector('.edibles', { timeout: 5_000 });
 	img = await page.screenshot();
 	await browser.close();
 	return new Response(img, {
